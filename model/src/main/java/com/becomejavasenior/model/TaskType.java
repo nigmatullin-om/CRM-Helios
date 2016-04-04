c5d222bda7f32eb14b88ffd149c19b7d42414c82
Igor Chaiun <igorek88@gmail.com>
2016-04-04 21:40:01 +0300

    Helios-41 fix task type

--- model/src/main/java/com/becomejavasenior/model/TaskType.java
+++ model/src/main/java/com/becomejavasenior/model/TaskType.java
@@ -1,7 +1,34 @@
 package com.becomejavasenior.model;
 
+
 import java.io.Serializable;
 
-public enum TaskType implements Serializable {
-    FOLLOW_UP, MEETING, OTHER
+public class TaskType implements Serializable {
+    private int id;
+    private String typeName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    @Override
+    public String toString() {
+        return "TaskType{" +
+                "id=" + id +
+                ", typeName='" + typeName + '\'' +
+                '}';
+    }
+
 }
