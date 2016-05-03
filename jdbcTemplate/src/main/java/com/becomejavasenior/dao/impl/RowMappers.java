18ad6d2bbc1cbc7a647cd99ae4a3829ad528e847
omn69 <omnx69@gmail.com>
2016-05-03 13:14:01 +0300

    DealDaoImpl & RowMappers & merge with master

--- jdbcTemplate/src/main/java/com/becomejavasenior/dao/impl/RowMappers.java
+++ jdbcTemplate/src/main/java/com/becomejavasenior/dao/impl/RowMappers.java
@@ -50,4 +50,18 @@ public class RowMappers {
             return company;
         };
     }
+
+    public static RowMapper<Deal> dealRowMapper() {
+
+        return (resultSet, i) -> {
+            Deal deal = new Deal();
+            deal.setId(resultSet.getInt("id"));
+            deal.setName(resultSet.getString("name"));
+            deal.setBudget(resultSet.getBigDecimal("budget"));
+            deal.setDealStage(DealStage.getDealStageById(resultSet.getInt("stage_id")));
+            deal.setCreationDate(resultSet.getDate("date_create"));
+            deal.setDeleted(resultSet.getBoolean("deleted"));
+            return deal;
+        };
+    }
 }
